baa4fb9125a60881313110cb13d01ce7bc951db1
Neil <pilotnd1@gmail.com>
2022-03-06 21:08:33 -0500

    Added Controller.

---
+++ researchAuthor/Controllers/AuthorController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using researchAuthor.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace researchAuthor.Controllers
+{
+    public class AuthorController : Controller
+    {
+        private readonly pubsContext _db;
+
+        public AuthorController(pubsContext pubs)
+        {
+            _db = pubs;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Author> objAuthorList = _db.Authors;
+            return View(objAuthorList);
+        }
+    }
+}
